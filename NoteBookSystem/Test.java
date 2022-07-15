9cf77b7986a08bac86ae848ae94f397e88ae2a81
heliosend <108450943+heliosend@users.noreply.github.com>
2022-07-16 00:04:24 +0800

    Update Test.java
    1. 添加了对对象数组的学习与测试。
    2.注意在定义了一个对象数组之后，数组中存放的是地址，对象还没有被实例化，需要进行new一个十里出来才能使用，详见代码43行及后续。
    3. 添加了Object类的学习。

--- NoteBookSystem/Test.java
+++ NoteBookSystem/Test.java
@@ -30,5 +30,32 @@ public class Test {
         for (String element : myNotebook.display()) {
             System.out.println(element);
         }
+        
+        //----------------对象数组的测试------------------------------
+        String[] s = new String[10];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = "" + i;
+        }
+        for (String element : s) {
+            System.out.println(element);
+        }
+
+        Value[] arrayOfValue=new Value[10];
+        for(int i=0;i< arrayOfValue.length;i++){
+            arrayOfValue[i]=new Value();
+            //————***注意***————
+            // 在定义了一个对象数组之后，还需要在每一步进行一次实体化，因为数组中存的只是分配好的地址，并没有一个实体
+            arrayOfValue[i].setValue(i);
+        }
+        for(Value element:arrayOfValue){
+            System.out.println(element.getValue());
+        }
+
+        //----------------Object类的学习---------------------------
+        Object obj=new Object();
+        obj="ss";    obj=1;    obj=2.3;    obj='s';    obj=true;
+        //Object类，表明是任意类型的变量，可以任意赋值
+
+
     }
 }
